1a45d2d3981c4598517f6fa5d2a542d5078641be
Pamela! <pamelam@uark.edu>
2018-07-24 13:15:41 -0500

    starting pages site

---
+++ scripts/presentation.js
@@ -0,0 +1,35 @@
+$('#orals').on('click', function() {
+    $('#oralPresentation').show();
+})
+
+$('#orals').on('click', function() {
+    $('#guestLectures').hide();
+})
+
+$('#orals').on('click', function() {
+    $('#posterPresentation').hide();
+})
+
+$('#poster').on('click', function() {
+    $('#posterPresentation').show();
+})
+
+$('#poster').on('click', function() {
+    $('#oralPresentation').hide();
+})
+
+$('#poster').on('click', function() {
+    $('#guestLectures').hide();
+})
+
+$('#guest').on('click', function() {
+    $('#guestLectures').show();
+})
+
+$('#guest').on('click', function() {
+    $('#oralPresentation').hide();
+})
+
+$('#guest').on('click', function() {
+    $('#posterPresentation').hide();
+})
